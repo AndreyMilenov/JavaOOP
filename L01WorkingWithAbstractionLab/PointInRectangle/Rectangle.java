ecd11ee0944f4af7bf0a4384442c4cfafc4bc4be
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-12 19:26:01 +0200

    Add files via upload

---
+++ L01WorkingWithAbstractionLab/PointInRectangle/Rectangle.java
@@ -0,0 +1,15 @@
+package L01WorkingWithAbstractionLab.PointInRectangle;
+
+public class Rectangle {
+    private final Point pointA;
+    private final Point pointC;
+
+    public Rectangle(Point pointA, Point pointC) {
+        this.pointA = pointA;
+        this.pointC = pointC;
+    }
+
+    public boolean isInside(Point p) {
+        return p.isGreaterThanOrEqual(pointA) && p.isLessThanOrEqual(pointC);
+    }
+}
