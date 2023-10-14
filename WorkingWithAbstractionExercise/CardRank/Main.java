0f26eea1b7b3db71ae9686ed15fad1c528b50ae5
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-14 16:41:42 +0200

    Add files via upload

---
+++ WorkingWithAbstractionExercise/CardRank/Main.java
@@ -0,0 +1,14 @@
+package WorkingWithAbstractionExercise.CardRank;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Card Ranks:");
+        for (CardRank cardRank : CardRank.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n",cardRank.ordinal(),cardRank.name());
+        }
+
+    }
+}
