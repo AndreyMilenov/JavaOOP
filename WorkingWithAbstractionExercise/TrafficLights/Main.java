a2d97c0276af01ca80230582533eae3c0f24427f
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-14 17:58:55 +0200

    Add files via upload

---
+++ WorkingWithAbstractionExercise/TrafficLights/Main.java
@@ -0,0 +1,31 @@
+package WorkingWithAbstractionExercise.TrafficLights;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Color[] signals = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Color::valueOf).toArray(Color[]::new);
+
+        List<TrafficLight> trafficLightList = new ArrayList<>();
+
+        for (Color color:signals) {
+            TrafficLight trafficLight = new TrafficLight(color);
+            trafficLightList.add(trafficLight);
+
+        }
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            for (TrafficLight trafficLight : trafficLightList) {
+                trafficLight.changeColor();
+                System.out.print(trafficLight);
+            }
+            System.out.println();
+        }
+    }
+}
