fe73df849493be087fd859c0297a1411f06441c0
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-14 17:13:12 +0200

    Add files via upload

---
+++ WorkingWithAbstractionExercise/CardsWithPower/Card.java
@@ -0,0 +1,16 @@
+package WorkingWithAbstractionExercise.CardsWithPower;
+
+
+
+public class Card {
+    private CardSuit cardSuit;
+    private CardRank cardRank;
+
+    public Card(CardSuit cardSuit, CardRank cardRanks) {
+        this.cardSuit = cardSuit;
+        this.cardRank = cardRanks;
+    }
+    public int getPower(){
+        return cardRank.getRankPower() + cardSuit.getSuitPower();
+    }
+}
