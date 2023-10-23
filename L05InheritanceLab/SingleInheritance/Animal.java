0c83bb2783c5fd45c925c252a392a05db0d51050
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-23 15:57:35 +0200

    Add files via upload
    Simple example for inheritance!

---
+++ L05InheritanceLab/SingleInheritance/Animal.java
@@ -0,0 +1,7 @@
+package L05InheritanceLab.SingleInheritance;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ L05InheritanceLab/SingleInheritance/Dog.java
@@ -0,0 +1,7 @@
+package L05InheritanceLab.SingleInheritance;
+
+public class Dog extends Animal{
+public void bark() {
+    System.out.println("barking...");
+}
+}
