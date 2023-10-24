f29d652620b7a4b5071ebb97b4a5a2e2fe850344
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-24 18:32:12 +0200

    Add files via upload

---
+++ L06InheritanceExercises/Animals/Dog.java
@@ -0,0 +1,11 @@
+package L06InheritanceExercises.Animals;
+
+public class Dog extends Animal{
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+    public String produceSound() {
+        return "Woof!";
+    }
+
+}
---
+++ L06InheritanceExercises/Animals/Kitten.java
@@ -0,0 +1,14 @@
+package L06InheritanceExercises.Animals;
+
+
+
+public class Kitten extends Cat {
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+    }
+    @Override
+    public String produceSound() {
+        return "Meow";
+    }
+
+}
