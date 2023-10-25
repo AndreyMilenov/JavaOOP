45ae2495e2fd83e3c663edfee1218da7ec077aec
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2023-10-25 12:30:23 +0200

    Add files via upload
    Simple example for using Interfaces.

---
+++ L07InterfacesAndAbstractionLab/CarShop/Car.java
@@ -0,0 +1,13 @@
+package L07InterfacesAndAbstractionLab.CarShop;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    int TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String getCountryProduced();
+
+}
